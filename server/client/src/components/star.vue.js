7e96ec5c4f0bf4b4f0d08f72f155f50bdcff6407
Maerak <maonajma@gmail.com>
2022-03-28 21:52:56 +0200

    STARS

--- server/client/src/components/star.vue.js
+++ server/client/src/components/star.vue.js
@@ -4,7 +4,7 @@ Vue.component('star', {
       <div  id="star" class="star">
           <div class="carousel-item">
             <div class="row">
-              <div class="col s12 m8">
+              <div class="col s12 m8 l8">
                 <div class="card blue-grey darken-1">
                   <div class="card-content white-text">
                     <span class="card-title">Card Title</span>
